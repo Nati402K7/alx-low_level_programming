285e5115753140e0a609514601be266c95f2a617
Nati402K7 <natnaeleyasu402@gmail.com>
2023-08-10 09:25:28 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  * Description: A C program that prints using the function printf
- * Return: o
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
 }
