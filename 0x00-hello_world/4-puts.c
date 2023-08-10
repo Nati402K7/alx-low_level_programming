eaddb6a9a1094655283d4ffed76d41c37bcfd73d
Nati402K7 <natnaeleyasu402@gmail.com>
2023-08-10 08:05:33 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: A program that prints using the function puts
+ * Return: Alawys 0 (success)
+*/
+int main(void)
+{
+puts('"programming is like building a multilingual puzzle');
+return (0);
+}
