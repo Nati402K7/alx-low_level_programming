904362966e943cbc6064d450739e11ca84392ac5
Nati402K7 <natnaeleyasu402@gmail.com>
2023-08-17 05:40:13 -0700

    a

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,7 +16,7 @@ void print_diagonal(int n)
 		for (post = 1; post <= n; post++)
 		{
 			for (space = 1; space <= post; space++)
-				_putchar(' ');
+				_putchar('');
 			_putchar(92);
 			_putchar('\n');
 		}
