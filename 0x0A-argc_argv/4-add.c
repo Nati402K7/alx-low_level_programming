ec1bb512315cca72ca77ecc23ee13a82812c36cf
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-04 07:52:05 -0700

    C

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+/**
+ * check_num - A function that check number
+ * @str: input
+ * Return: 1
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
 
 /**
  * main - A program that  adds positive numbersz.
@@ -12,32 +36,25 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
-	unsigned int k, sum = 0;
-	char *e;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	if (argc > 1)
+	count = 1;
+	while (count < argc)
 	{
-		for (i = 1; i < argc; i++)
+		if (check_num(argv[count]))
 		{
-			e = argv[i];
-
-			for (k = 0; k < strlen(e); k++)
-			{
-				if (e[k] < 48 || e[k] > 57)
-				{
-					 printf("Error\n");
-					 return (1);
-				}
-			}
-			sum += atoi(e);
-			e++;
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
 		}
-		printf("%d\n", sum);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
 	}
-	else
-	{
-		printf("0\n");
-	}
+	printf("%d\n", sum);
 	return (0);
 }
