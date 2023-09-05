7c40a5b38c1420d471b845338a275108e82d2be2
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-05 02:16:13 -0700

    It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - A function that frees a 2 dimentional grid
+ * @grid: input array
+ * @height: height of array
+ * Return: grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	for (; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -2,3 +2,4 @@ Float like a butterfly, string like a bee
 The woman who has no imagination has no wings
 He who is not courageous enough to take risks will accomplish nothing in life
 If you even dream of beating me you'd better wake up and apologize
+It's not bragging if you can back it up
