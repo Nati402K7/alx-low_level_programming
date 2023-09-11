ef5dd42dcee68ed9d522d71fc05b966c00ef7b48
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-11 07:00:24 -0700

    C - preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints the name of the file it was compiled.
+ * Return: Zero
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
