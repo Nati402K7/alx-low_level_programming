fd40b07db4d12f191867a70ec0241eeee59cad41
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-13 05:15:46 -0700

    C - function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - A function that add to numbers
+ * @a: first input
+ * @b: second input
+ * Return: sum of to numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - A function that subtractes to numbers
+ * @a: first input
+ * @b: second input
+ * Return: subtact of to numbers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - A function that multiplays to numbers
+ * @a: first input
+ * @b: second input
+ * Return: multiplaied of to numbers
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - A function that divided to numbers
+ * @a: first input
+ * @b: second input
+ * Return: divided of to numbers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - A function that return remainder of to numbers
+ * @a: first input
+ * @b: second input
+ * Return: remainder of to n umbers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
