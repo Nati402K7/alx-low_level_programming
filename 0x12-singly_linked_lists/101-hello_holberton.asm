0f5d953ffd81743b5b3c6a1fcbf0116379758533
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-21 08:32:07 -0700

    Real programmers can write assembly code in any language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,12 @@
+section		.text
+	global main
+	extern printf
+
+main:
+	mov	edi, now
+	mov	eax, 0
+	call	printf
+
+
+section		.data
+	now db 'Hello, Holberton', 0
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -4,3 +4,4 @@ Add node
 Add node at the end
 Free list
 The hare and the tortoise
+Real programmers can write assembly code in any language
