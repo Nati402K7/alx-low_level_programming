5005e60dc1a6a848f23540168b9fb4d3474d9cab
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-26 11:08:43 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *del, *node;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || !*head)
 		return (-1);
 
 	if (!index)
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,7 +11,7 @@ int pop_listint(listint_t **head)
 	listint_t *del;
 	int n;
 
-	if (!head)
+	if (!head || !*head)
 		return (0);
 
 	del = (*head)->next;
