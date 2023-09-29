f741dd775f26829d6e5d6db3936dcad8b6b38bc7
Nati402K7 <natnaeleyasu402@gmail.com>
2023-09-29 06:59:39 -0700

    C-bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * filp_bits - A function that returns the number of bits you would need tofilp
+ * flip_bits - A function that returns the number of bits you would need tofilp
  * @n: first input
  * @m: second input
  * Return: number of bits
  */
 
-unsigned int filp_bits(unsigned long int n, unsigned long int m)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int i = n ^ m;
 	unsigned int v = 0;
