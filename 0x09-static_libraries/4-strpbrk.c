47d7e687480b16f0b527b5ecb22825300eecca02
Nati402K7 <natnaeleyasu402@gmail.com>
2023-10-27 13:09:48 -0700

    C static libraries

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - A function that points
+ * @s: a char
+ * @accept: a number
+ * Return: pointer to the byte
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int itr, jtr;
+	char *p;
+
+	itr = 0;
+	while (s[itr] != '\0')
+	{
+		jtr = 0;
+		while (accept[jtr] != '\0')
+		{
+			if (accept[jtr] == s[itr])
+			{
+				p = &s[itr];
+				return (p);
+			}
+			jtr++;
+		}
+		itr++;
+	}
+	return (0);
+}
