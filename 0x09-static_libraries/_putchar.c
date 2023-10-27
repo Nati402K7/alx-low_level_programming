e7fccfe164c34ba3976de39ab36b7656a06bdbd5
Nati402K7 <natnaeleyasu402@gmail.com>
2023-10-27 13:13:58 -0700

    update

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -4,7 +4,7 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1.On error, -1 is returned, and errno set appropriately
+ * Return: On success 1.On error, -1 is returned, and errno appropriately
  */
 
 int _putchar(char c)
