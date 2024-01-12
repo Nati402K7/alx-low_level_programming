0e8ee47272f98a61ab6e201bbfc9a97436feb654
Nati402K7 <natnaeleyasu402@gmail.com>
2024-01-12 12:10:49 -0800

    update

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,7 @@
 /**
  * dlistint_len - A function that returns the number of elements in a linked dl
  * @h: input head node
- * Retrun: number of elements
+ * Return: number of elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
